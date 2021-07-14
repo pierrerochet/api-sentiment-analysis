18a684e2ff9f278eb9db21e0762ef1ef5c0fbeec
Pierre Rochet <pierre-rochet@outlook.fr>
2021-07-14 10:40:23 +0200

    update cors

--- app/main.py
+++ app/main.py
@@ -23,9 +23,9 @@ app = FastAPI(
 )
 
 origins = [
-    "http://localhost:8880",
-    "http://172.22.0.1",
-    'http://ec2-15-236-60-229.eu-west-3.compute.amazonaws.com'
+    "http://localhost",
+    # "http://172.22.0.1",
+    # 'http://ec2-15-236-60-229.eu-west-3.compute.amazonaws.com'
 ]
 
 app.add_middleware(
