81072c0e81a0780932723e5a941dcdc9db9d00aa
Pierre Rochet <pierre-rochet@outlook.fr>
2021-07-16 12:35:11 +0200

    update cors

--- app/main.py
+++ app/main.py
@@ -23,7 +23,8 @@ app = FastAPI(
 )
 
 origins = [
-    "http://localhost",
+    'https://www.pierrerochet.com'
+    # "http://localhost:3000",
     # "http://172.22.0.1",
     # 'http://ec2-15-236-60-229.eu-west-3.compute.amazonaws.com'
 ]
